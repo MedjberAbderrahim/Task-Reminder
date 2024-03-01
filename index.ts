5189b69c42c0ba2900a2045a10a9d9d0858933a8
Medjber Abderrahim <medjber.abderrahim@gmail.com>
2024-03-01 18:45:09 +0100

    Timer for task feature + minor improvements
    Added one of the remaining features, to set and check for the time of each task and send a notification (settled on an alert for now, will change it later), and added some minor changes, like disappearing input bar and selection tags, ...etc
    the selection tags still need some styling
    after this, I only need to set up the notifications (probably gonna go with the Notification API mentioned on MDN), and set the required measures for the transphormation to a chrome extension (setting manifest.json, and making changes to sizes after the window changes to an extension window)

--- index.ts
+++ index.ts
@@ -1,57 +1,115 @@
 const inputBar = document.querySelector("#input") as HTMLInputElement
-const addButton = document.querySelector("#addButton") as HTMLButtonElement
+const newTaskButton = document.querySelector("#newTask") as HTMLButtonElement
+const addTaskButton = document.querySelector("#addTask") as HTMLButtonElement
 const taskList = document.querySelector("#taskList") as HTMLDivElement
+const timerSelect = document.querySelector("#timerSelect") as HTMLSelectElement
+const hourSelect = document.querySelector("#hourSelect") as HTMLSelectElement
+const minuteSelect = document.querySelector("#minuteSelect") as HTMLSelectElement
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms)); // sleep function, similar to the one of C/C++
 
 let tasks: HTMLElement[] = []
 
-if(inputBar === null)
-    throw new Error("ERROR! inputBar is null");
+timerSelect.addEventListener("change", (): void =>{
+    if(timerSelect.value == "no"){
+        hourSelect.style.display = "none"
+        minuteSelect.style.display = "none"
+    }
+    else{
+        hourSelect.style.display = "initial"
+        minuteSelect.style.display = "initial"
+    }
+})
 
-if(addButton === null)
-    throw new Error("ERROR! addButton is null");
+newTaskButton.addEventListener("click", (): void => {
+    inputBar.style.display = "initial"
+    newTaskButton.style.display = "none"
+    addTaskButton.style.display = "initial"
+    timerSelect.style.display = "initial"
+})
 
-if(taskList === null)
-    throw new Error("ERROR! taskList is null");
-
-addButton?.addEventListener("click", (): void => {
-    if(inputBar.value === "")
-        return
-    
+function createTask(text: string): HTMLDivElement {
     let newTask = document.createElement("div") as HTMLDivElement
-    newTask.className = "taskElement"
-    
     let newRow = document.createElement("div") as HTMLDivElement
-    newRow.textContent = inputBar.value 
-
+    let newDiv = document.createElement("div") as HTMLDivElement
+    let newSpan = document.createElement("span") as HTMLSpanElement
     let newRemoveButton = document.createElement("button") as HTMLButtonElement
+
+    newTask.className = "taskElement"
+
+    newRow.textContent = text
+
+    newSpan.className = "timeElements"
+
+    if(timerSelect.value == "yes")
+        newSpan.textContent = `${hourSelect.value}:${minuteSelect.value}`
+    else
+        newSpan.textContent = ""
+
     newRemoveButton.className = "removeButton"
     newRemoveButton.setAttribute("onclick", "removeTask(this)")
 
-    newTask.append(newRow, newRemoveButton)
-    taskList.append(newTask)
+    newDiv.append(newSpan, newRemoveButton)
 
+    newTask.append(newRow, newDiv)
+
+    return newTask
+}
+
+addTaskButton.addEventListener("click", (): void => {
+    if(inputBar.value === "")
+        return
+
+    let newTask = createTask(inputBar.value)
+
+    taskList.append(newTask)
     tasks.push(newTask)
-    
+
     inputBar.value = ""
+
+    addTaskButton.style.display = "none"
+    inputBar.style.display = "none"
+    newTaskButton.style.display = "initial"
+    timerSelect.style.display = "none"
+    hourSelect.style.display = "none"
+    minuteSelect.style.display = "none"
+    timerSelect.value = "no"
 })
 
-async function removeTask(taskElement: HTMLElement): Promise<void>{
-    let parentNode = taskElement.parentElement
-    if(parentNode === null)
-        throw new Error(`${taskElement}.parentNode is null`);
+async function removeTask(taskElement: HTMLButtonElement): Promise<void>{
+    let divNode = taskElement.parentElement as HTMLDivElement
+    let parentNode = divNode.parentElement as HTMLDivElement
+    let rowNode: HTMLElement = parentNode.children[0] as HTMLDivElement
 
     taskElement.textContent = 'X'
 
-    let rowNode: HTMLElement = parentNode.children[0] as HTMLDivElement
     rowNode.style.textDecoration = "line-through"
     rowNode.style.opacity = "40%"
 
-    taskElement.style.opacity = "40%"
+    divNode.style.opacity = "40%"
 
     await sleep(2000)
 
     tasks.splice(tasks.indexOf(parentNode), 1)
     parentNode.remove()
 }
+
+function checkTimers() {
+    let time = new Date()
+
+    for (const index in tasks){
+        let spanElement = tasks[index].children[1].children[0] as HTMLSpanElement
+
+        if(spanElement.textContent == "")
+            continue
+
+        if(time.getHours() == Number(spanElement.textContent?.slice(0, 2)) && time.getMinutes() == Number(spanElement.textContent?.slice(-2)) ){
+            let alertText = tasks[index].children[0].textContent
+
+            removeTask(tasks[index].children[1].children[1] as HTMLButtonElement)
+            alert(`Task is up!\n${alertText}`)
+        }
+    }
+}
+
+setInterval(checkTimers, 1000)
